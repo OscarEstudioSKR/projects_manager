e96fbabe03cb3b6d0316482a0302f97ed34c5ae4
Skr <oscaraparisisancho@gmail.com>
2019-02-12 09:28:44 +0100

    Updates

---
+++ precache-manifest.2ef4438c6368d7f12a14109deb7cea4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7280a071eda42ad4d1ea",
+    "url": "/projects_manager/static/css/main.f758a79a.chunk.css"
+  },
+  {
+    "revision": "7280a071eda42ad4d1ea",
+    "url": "/projects_manager/static/js/main.7280a071.chunk.js"
+  },
+  {
+    "revision": "66d3c9ebf0309434c657",
+    "url": "/projects_manager/static/js/runtime~main.66d3c9eb.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/projects_manager/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/projects_manager/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d7bd40b36fb74168682a11c806ae2b20",
+    "url": "/projects_manager/index.html"
+  }
+];
