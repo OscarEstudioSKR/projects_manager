8314352f18a2e4fdbcea5103c48246d1eeaf6d43
Skr <oscaraparisisancho@gmail.com>
2019-02-12 12:46:17 +0100

    complemento Buscador

--- src/elements/js/Buscador.js
+++ src/elements/js/Buscador.js
@@ -3,10 +3,37 @@ import lupa from '../../img/lupa.png';
 
 
 class Buscador extends Component {
+
+    constructor(props){
+      super(props);
+      this.state= {
+        search: "",
+      }
+    }
+    onChange(e){
+      this.setState({
+        search: e.target.value,
+      })
+    }
+
+    buscador(e){
+      console.log("buscar: "+this.state.search);
+      e.preventDefault();
+    }
+
     render() {
       return (
         <div className="Buscador">
-            <img src={lupa}/>
+          <button 
+            onClick={this.buscador.bind(this)}>
+              <img src={lupa}/>
+          </button>
+          <input 
+            name="input-search-name" 
+            value={this.state.search} 
+            id="input-search"
+            onChange= {this.onChange.bind(this)} 
+            placeholder="Buscador..." />   
         </div>
       );
     }
